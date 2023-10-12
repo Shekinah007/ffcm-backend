65500b7283d3f1864aed9d9ffbdbf2863d467d34
Shekinah Shalom-Hope <shalomsheks@gmail.com>
2023-10-12 15:38:45 +0100

    Install @nestjs/config to configure environmental variables - Switch to mongo atlas

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,15 @@ import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb://0.0.0.0:27017/ffcm"), AuthModule, UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    // MongooseModule.forRoot("mongodb://0.0.0.0:27017/ffcm"),
+    MongooseModule.forRoot('mongodb+srv://shalomsheks:shekssheks@nodetuts.m5oxomk.mongodb.net/ffcm'),
+    AuthModule, UserModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
